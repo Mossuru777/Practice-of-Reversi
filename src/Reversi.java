69ab856209294e3fb37791737819243c6b87072b
Mossuru777 <mossuru777@gmail.com>
2017-03-31 03:28:24 +0900

    PlayerクラスでUserクラスインスタンスをを弱参照で保持しているため、Reversiクラスのmainメソッド内での強参照がなくなってGCされた後に、操作不能となってしまう問題に対処

--- src/Reversi.java
+++ src/Reversi.java
@@ -5,16 +5,16 @@ import net.mossan.java.reversi.model.user.GUIConsoleUser;
 import javax.swing.*;
 
 public class Reversi extends JPanel {
+    // Two gui console user
+    private static final GUIConsoleUser USER1 = new GUIConsoleUser();
+    private static final GUIConsoleUser USER2 = new GUIConsoleUser();
+
     public static void main(String[] args) {
         // 8x8, 50px x 50px per cell, "Reversi" window title
         Game game = new Game(8);
         BoardDrawer board = new BoardDrawer(game, 50, "Reversi");
         game.addEventListener(board);
-
-        // Two gui console user
-        GUIConsoleUser player1 = new GUIConsoleUser();
-        GUIConsoleUser player2 = new GUIConsoleUser();
-        game.addUser(player1);
-        game.addUser(player2);
+        game.addUser(USER1);
+        game.addUser(USER2);
     }
 }
